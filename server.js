e4ab19bf9b0751fe0315601d90a25017af0ec843
celinefigo-dev <cmafigu@byupathway.edu>
2026-03-14 20:40:35 +0200

    fix render port issue

--- server.js
+++ server.js
@@ -6,25 +6,41 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+/*************************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+app.set("views", "./views")
+
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
+
+
+//Index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
+const port = process.env.PORT || 5500
 const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`Server running on port ${port}`)
 })
