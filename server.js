719c5b4579b02ca9246707922ddc2ec96924c554
celinefigo-dev <cmafigu@byupathway.edu>
2026-03-16 19:32:35 +0200

    add wk 01changes

--- server.js
+++ server.js
@@ -1,94 +1,39 @@
 /* ******************************************
- * This server.js file is the primary file of the
+ * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const env = require("dotenv").config();
-const app = express();
-const static = require("./routes/static");
-const session = require("express-session");
-
-
-/* ***********************
- * View Engines and Templates
- *************************/
-app.set("view engine", "ejs");
-app.use(expressLayouts);
-app.set("layout", "./layouts/layout");
-app.set("views", "./views");
-
-/* ***********************
- * Middleware
- * ************************/
-app.use(
-  session({
-    store: new (require("connect-pg-simple")(session))({
-      createTableIfMissing: true,
-      pool,
-    }),
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    name: "sessionId",
-  })
-);
-
-// For cookie
-app.use(cookieParser());
-
-// Express Messages Middleware
-app.use(require("connect-flash")());
-app.use(function (req, res, next) {
-  res.locals.messages = require("express-messages")(req, res);
-  next();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-app.use(utilities.checkJWTToken);
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+const env = require("dotenv").config()
+const app = express()
+const static = require("./routes/static")
 
 /* ***********************
  * Routes
  *************************/
-app.use(static);
 
-// Index route
-app.get("/", utilities.handleErrors(baseController.buildHome));
-
-// Inventory Route
-app.use("/inv", inventoryRoute);
-
-// Account Route
-app.use("/account", accountRoute);
-
-//Appointment Route
-app.use("/appointments", appointmentRoute);
-
-// File Not Found Route - must be last route in list
-app.use(async (req, res, next) => {
-  next({
-    status: 404,
-    message: "Sorry, you must have been in the wrong direction.",
-  });
-});
-
-app.use("/test", inventoryRoute);
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+app.use(static)
+//Index route
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT;
-const host = process.env.HOST;
+const port = process.env.PORT
+const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
-});
+  console.log(`app listening on ${host}:${port}`)
+})
