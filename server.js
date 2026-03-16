87f9775edced2ffd36fd1a012f54e8c1ffd891f6
celinefigo-dev <cmafigu@byupathway.edu>
2026-03-16 18:30:42 +0200

    add changes

--- server.js
+++ server.js
@@ -10,111 +10,39 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const accountRoute = require("./routes/accountRoute")
-const errorRoute = require("./routes/errorRoute")
-const reviewRoute = require("./routes/reviewRoute")
-const utilities = require("./utilities")
-const session = require("express-session")
-const pool = require("./database")
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
 
-/* ***********************
- * Middleware
- *************************/
-app.use(session({
-  store:new (require('connect-pg-simple')(session))({
-    createTableIfMissing:true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
-  resave:true,
-  saveUninitialized:true,
-  name:'sessionId'
-}))
 
-//Express Messages Middleware
-app.use(require('connect-flash')())
-app.use(function(req,res,next){
-  res.locals.messages = require('express-messages')(req,res)
-  next()
-})
-
-//Body-Parser Middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
-//Cookie
-app.use(cookieParser())
-
-app.use(utilities.checkJWTToken)
-
-/* ***********************
+/*************************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout")   //not at views root
+app.set("layout", "./layouts/layout")
+const path = require("path")
+app.set("views", path.join(__dirname, "views"))
+
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 
-//Index Route
-app.get("/", utilities.handleErrors(baseController.buildHome))
 
-//Inventory Routes
-app.use("/inv", inventoryRoute)
-
-//Account Routes
-app.use("/account",accountRoute)
-
-//Reviews Routes
-app.use("/reviews",reviewRoute)
-
-//Error Route
-app.use("/err",errorRoute)
-
-// File Not Found Route - must be last route in list
-app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
-})
-
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
-app.use(async (err, req, res, next)=>{
-
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  
-  if(err.status==404)
-    message = err.message
-  else
-    message = 'Oh no! There was a crash. Maybe try a different route?'
-
-  res.render("errors/error",{
-    title: err.status || "Server Error",
-    message,
-    nav
-  })
-
+//Index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
 })
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
+const port = process.env.PORT || 5500
 const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`Server running on port ${port}`)
 })
