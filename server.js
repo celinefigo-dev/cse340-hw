4eee75fedf257f17bff1422d2090c64caf0711f3
celinefigo-dev <cmafigu@byupathway.edu>
2026-03-28 20:52:47 +0200

    add changes MVC WK3

--- server.js
+++ server.js
@@ -55,8 +55,8 @@ app.use(async (err, req, res, next) => {
  * Local Server Information36
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
